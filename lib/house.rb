9e24050de025df5a8a8f633693c4ea1d70a52e2f
Connor Ferguson <cpfergus1@gmail.com>
2020-08-26 09:36:43 -0600

    Implemented add room method for house class

--- lib/house.rb
+++ lib/house.rb
@@ -3,4 +3,15 @@ require './lib/room'
 
 class House
 
+  attr_reader :price, :address, :rooms
+
+  def initialize(price, address)
+    @price = price
+    @address = address
+    @rooms = []
+  end
+
+  def add_room(room)
+    rooms << room
+  end
 end
