a1c4495ece9a6425dd1d41bcfccbb4d9cff57052
Connor Ferguson <cpfergus1@gmail.com>
2020-08-26 10:17:48 -0600

    Added test for house details

--- test/house_test.rb
+++ test/house_test.rb
@@ -59,4 +59,10 @@ class HouseTest <Minitest::Test
     house.add_room(room_4)
     assert_equal 1900 , house.area
   end
+
+  def test_house_details
+    house = House.new("$400000", "123 sugar lane")
+    expected = {"price" => 400000, "address" => "123 sugar lane"}
+    assert_equal expected, house.details
+  end
 end
