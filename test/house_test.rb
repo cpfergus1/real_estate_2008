9d81bd75c184a273633db286518ddd8c9c5eebf4
Connor Ferguson <cpfergus1@gmail.com>
2020-08-26 09:31:42 -0600

    Add attributes test for house class

--- test/house_test.rb
+++ test/house_test.rb
@@ -10,6 +10,13 @@ class HouseTest <Minitest::Test
     assert_instance_of House, house
   end
 
+  def test_it_has_attributes
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal "$400000", house.price
+    assert_equal "123 sugar lane", house.address
+    assert_equal [], house.rooms
+  end
+
   
 
 end
